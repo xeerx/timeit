a5746681e732628dda444ec5e0c29e172121f157
xeerx <88335378+xeerx@users.noreply.github.com>
2022-07-05 20:56:08 +0200

    0.8
    change algorithm to find median result
    change some classes members to be temp variables in functions
    output with colors on unix os, else wirhout colors
    add find_median() function

--- timeit.hpp
+++ timeit.hpp
@@ -1,7 +1,7 @@
 /**
  * @file    timeit.hpp
  * @brief   A quick C++ library to measure/compare code execution time
- * @version 0.7.7
+ * @version 0.8
  *
  * Copyright (c) 2022 Maysara Elshewehy (xeerx.com) (maysara.elshewehy@gmail.com)
  *
@@ -19,12 +19,22 @@
 #include <stdexcept>    // std::runtime_error
 #include <utility>      // std::pair
 
+double find_median(std::vector<std::size_t> & v) noexcept(false)  
+{
+    if(v.empty()) throw std::runtime_error("The vector must not be empty");
+    
+    const auto middleItr = v.begin() + v.size() / 2;
+    std::nth_element(v.begin(), middleItr, v.end());
+    if (v.size() % 2 == 0) {
+    const auto leftMiddleItr = std::max_element(v.begin(), middleItr);
+    return (*leftMiddleItr + *middleItr) / 2.0; } else { return *middleItr; }
+}
+
 class timeit
 {
     using clock = std::chrono::steady_clock;
 
     private: 
-    std::vector<std::size_t> results {};
     std::size_t result = 0;
 
     public:
@@ -32,6 +42,7 @@ class timeit
     timeit(const std::size_t count, const F func, A&& ...args) noexcept(false) 
     {
         if(count == 0) throw std::runtime_error("The count must not be zero");
+        std::vector<std::size_t> results {};
 
         for (std::size_t i = 0; i < count; i++)
         {
@@ -41,11 +52,7 @@ class timeit
             results.push_back((end - begin).count());
         }
 
-        // get median number in vector, this is the best way i found to get the best result !
-        std::nth_element(results.begin(), results.begin() + results.size()/2, results.end());
-        result = results[results.size()/2];
-
-        results.clear();
+        result = find_median(results);
     }
 
     std::size_t nanoseconds () { return result;                  }
@@ -68,44 +75,57 @@ class compareit
     using clock = std::chrono::steady_clock;
 
     private:
-    std::vector<std::size_t> results1 {}, results2 {};
-    std::size_t result1 = 0, result2 = 0;
+    std::size_t func1_result = 0, func2_result = 0;
 
     template <class F>
-    void calc(std::vector<std::size_t>& results, std::size_t& result, const std::size_t count, const F func) noexcept
+    void calc(std::size_t& result, const std::size_t count, const F func) noexcept
     {
+        std::vector<std::size_t> results {};
+
         for (std::size_t i = 0; i < 10; i++) results.push_back(timeit(count,func).nanoseconds());
 
-        std::nth_element(results.begin(), results.begin() + results.size()/2, results.end());
-        result = results[results.size()/2];
-
-        results.clear();
+        result = find_median(results);
     }
 
     void handler() noexcept
     {
-        if(result1 == result2) 
+        if(func1_result == func2_result) 
         {
-            std::cout 
-            << "\033[1;33m" << "[COMPARE IT]" << "\033[0m"
-            << "\033[0;34m" << " first(" << "\033[1;35m" << result1 << "\033[0;34m" << ") " << "\033[0m"
-            << "\033[1;33m" << "=" << "\033[0m"
-            << "\033[0;34m" << " second(" << "\033[1;35m" << result2 << "\033[0;34m" << ") " << "\033[0m \n";
+            #ifdef __unix__
+                std::cout 
+                << "\033[1;33m" << "[COMPARE IT]" << "\033[0m"
+                << "\033[0;34m" << " first(" << "\033[1;35m" << func1_result << "\033[0;34m" << ") " << "\033[0m"
+                << "\033[1;33m" << "=" << "\033[0m"
+                << "\033[0;34m" << " second(" << "\033[1;35m" << func2_result << "\033[0;34m" << ") " << "\033[0m \n";
+            #else
+                std::cout << "[COMPARE IT]" << " first(" << func1_result << ") = second(" << func2_result << ") \n";
+            #endif
+
             return;
         }
 
-        const std::pair<std::size_t, std::size_t> bounds = std::minmax(result1,result2);
+        const std::pair<std::size_t, std::size_t> bounds = std::minmax(func1_result,func2_result);
         
         const std::size_t xtimes = bounds.first == 0 ? 1 : (bounds.second / bounds.first);
         
-        const bool first_is_max = bounds.second == result1;
+        const bool first_is_max = bounds.second == func1_result;
 
-        std::cout 
-        << "\033[1;33m" << "[COMPARE IT]" << "\033[0m"
-        << "\033[0;34m" << (first_is_max ? " first(" : " second(")  << "\033[1;35m"  << (first_is_max? result1 : result2) << "\033[0;34m" << ") " << "\033[0m"
-        << "\033[1;33m" << ">" << "\033[0m"
-        << "\033[0;34m" << (first_is_max ? " second(" : " first(")  << "\033[1;35m" << (first_is_max ? result2 : result1) << "\033[0;34m" << ") " << "\033[0m"
-        << "\033[1;31m" << "x" << std::fixed << std::setprecision(3) << xtimes << "\033[0m \n";
+        #ifdef __unix__
+            std::cout 
+            << "\033[1;33m" << "[COMPARE IT]" << "\033[0m"
+            << "\033[0;34m" << (first_is_max ? " first(" : " second(")  << "\033[1;35m"  << (first_is_max? func1_result : func2_result) << "\033[0;34m" << ") " << "\033[0m"
+            << "\033[1;33m" << ">" << "\033[0m"
+            << "\033[0;34m" << (first_is_max ? " second(" : " first(")  << "\033[1;35m" << (first_is_max ? func2_result : func1_result) << "\033[0;34m" << ") " << "\033[0m"
+            << "\033[1;31m" << "x" << std::fixed << std::setprecision(3) << xtimes << "\033[0m \n";
+        #else
+            std::cout 
+            << "[COMPARE IT]" 
+            << (first_is_max ? " first(" : " second(") << (first_is_max? func1_result : func2_result)<< ") >"
+            << (first_is_max ? " second(" : " first(") << (first_is_max ? func2_result : func1_result) << ") x"
+            << std::fixed << std::setprecision(3) << xtimes << "\n";
+        #endif
+        
+        return;
     }
 
     public:
@@ -114,8 +134,8 @@ class compareit
     {
         if(count == 0) throw std::runtime_error("The count must not be zero");
 
-        calc(results1, result1, count, func1);
-        calc(results2, result2, count, func2);
+        calc(func1_result, count, func1);
+        calc(func2_result, count, func2);
         handler();
     }
 };
